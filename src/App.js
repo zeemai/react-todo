1ee9a48943990e60d387f52536c7decd73c00324
Zee Mai <zihao_mai@hotmail.com>
2020-06-10 14:07:22 +0100

    files clean up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+export const App = () => (
     <div className="App">
-
+      <h1>hey</h1>
     </div>
-  );
-}
-
-export default App;
+  )
